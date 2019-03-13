a80dfb6335ab287aa74f967593e0a649a57bd45d
xishan <zhangxishan@mgzf.com>
2019-03-13 13:51:11 +0800

    spring cloud  eureka server

--- spring-cloud-all/spring-cloud-eureka-peer1/src/main/java/com/elgin/study/spring/cloud/eureka/peer1/SpringCloudEurekaApplication.java
+++ spring-cloud-all/spring-cloud-eureka-peer1/src/main/java/com/elgin/study/spring/cloud/eureka/peer1/SpringCloudEurekaApplication.java
@@ -6,10 +6,10 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
 @EnableEurekaServer
-public class SpringCloudEurekaPeer1Application {
+public class SpringCloudEurekaApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SpringCloudEurekaPeer1Application.class, args);
+		SpringApplication.run(SpringCloudEurekaApplication.class, args);
 	}
 
 }
